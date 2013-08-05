5fcc075f8e77c06bc7376dc0c3ff96cfd5dcfa22
jczetta <jczetta@gmail.com>
2013-08-05 15:29:22 -0400

    efforts toward adding stats
    more intricate queries. this commit at broken state.

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,6 @@
 // globals (?)
 var mysql = require('mysql');
+
 var DATABASE = 'oerpublish';
 var conn = mysql.createConnection({
 	user: 'USR',
@@ -9,6 +10,7 @@ var conn = mysql.createConnection({
 });
 conn.connect();
 
+var course_details;
 // exports.findAll = function(req,res) {
 // 	res.send([{name:ab,description:"fake super intro class", professor:"Dr. OM"}, {name:'200'}, {name:'300'}]);
 // };
@@ -16,14 +18,14 @@ conn.connect();
 
 exports.findAll = function(req,res) {
 	var course_details = new Array();
-	var results, courseamt;
+	var courseamt;
+	var results, stats_res;
 	//console.log(conn);
 	conn.query('USE ' + DATABASE);
-	conn.query('SELECT field_course_code_value AS name FROM content_type_course WHERE field_course_code_value IS NOT NULL GROUP BY field_course_code_value',
+	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid as nid FROM content_type_course WHERE field_course_code_value IS NOT NULL AND content_type_course.field_content_type_value LIKE 'course' GROUP BY field_course_code_value",
 		function(err, rows, fields) {
 			if (err) throw err;
-			// console.log('Result is: ', rows[0].solution);
-			// console.log('There are ', rows.length, 'nodes of type course');
+
 			courseamt = rows.length;
 			results = rows;
 			console.log(results);
@@ -31,10 +33,18 @@ exports.findAll = function(req,res) {
 			for (row in results) {
 				course_details.push(results[row]);
 			}
-			res.send(course_details);
+			//res.send(course_details);
 		}
 	);
+
+	//conn.query("SELECT totalviews AS views, nid as nid FROM oer_analytics_courses WHERE ")
+
+	for (x in course_details) {
+		x["views"] = 
+	}
 };
+
+
 
 exports.findByUnit = function(req, res) {
 	res.send({unit:req.params.unit, name: "SI 101", description: "a fake intro class"});
