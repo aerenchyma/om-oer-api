f897ca0634a1bd6a6769e4d18ab038b29f18be98
jczetta <jczetta@gmail.com>
2013-08-06 12:04:52 -0400

    added amt, youtube bool
    and some comments about problems and ideas

--- routes/courses.js
+++ routes/courses.js
@@ -31,7 +31,54 @@ exports.findAll = function(req,res) {
 	);
 };
 
+exports.youtubeBool = function(req, res) { // issue -- the param in this REST URL does not look like your usual request, which is bad.
+	var course_details = new Array();
+	var results, courseamt;
+	var yt = req.params.youtube;
+	conn.query('USE ' + DATABASE);
+	if (yt == 'vids') {
+		conn.query('SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, oer_analytics_youtube.totalviews AS youtube_views FROM content_type_course RIGHT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value',
+			function(err, rows, fields) {
+				if (err) throw err;
+				// console.log('Result is: ', rows[0].solution);
+				// console.log('There are ', rows.length, 'nodes of type course');
+				courseamt = rows.length;
+				results = rows;
+				console.log(results);
+				//course_details.push({number_of_courses:courseamt}); // hmm
+				for (row in results) {
+					course_details.push(results[row]);
+				}
+				res.send({courses_with_youtube: courseamt, courses: course_details});
+			}
+		);
+	}
+	else if (yt == 'novids') {
+		conn.query('SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, oer_analytics_youtube.totalviews AS youtube_views FROM content_type_course LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid = content_type_course.nid WHERE oer_analytics_youtube.course_nid IS null AND field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value',
+			function(err, rows, fields) {
+				if (err) throw err;
+				// console.log('Result is: ', rows[0].solution);
+				// console.log('There are ', rows.length, 'nodes of type course');
+				courseamt = rows.length;
+				results = rows;
+				console.log(results);
+				//course_details.push({number_of_courses:courseamt}); // hmm
+				for (row in results) {
+					course_details.push(results[row]);
+				}
+				res.send({courses_without_youtube: courseamt, courses: course_details});
+			}
+		);
+	}
+
+}
+
 //reminder for other piece -- possible exp to diff file
+//find in a certain way
 exports.findByUnit = function(req, res) {
 	res.send({unit:req.params.unit, name: "SI 101", description: "a fake intro class"});
 };
+
+// could do a minimum edit distance for name, maybe? is that useful? within a filter or something? or just a parameter..
+
+// also need larger aggregation. bah this architecture :/
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
 var app = express();
 
 app.get('/courses', courses.findAll);
-app.get('/courses/:date', courses.findByUnit)
+app.get('/courses/:youtube', courses.youtubeBool);
+
+app.get('/courses/:unit', courses.findByUnit)
 
 app.listen(3000);
 console.log("Listening on port 3000...");
