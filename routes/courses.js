4f0519c019beadd293f72a71cc2d18d644f62843
jczetta <jczetta@gmail.com>
2013-08-07 13:14:06 -0400

    Creator added to course details
    also some comment cleanup

--- routes/courses.js
+++ routes/courses.js
@@ -15,7 +15,7 @@ exports.findAll = function(req,res) {
 	var results, courseamt;
 	//console.log(conn);
 	conn.query('USE ' + DATABASE);
-	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value",
+	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value",
 
 		function(err, rows, fields) {
 			if (err) throw err;
@@ -30,7 +30,7 @@ exports.findAll = function(req,res) {
 	);
 };
 
-exports.youtubeBool = function(req, res) { // issue -- the param in this REST URL does not look like your usual request/params structure. should test with app or something.
+exports.youtubeBool = function(req, res) { // issue -- the param in this REST URL is unlike usual request/params structure.
 	var course_details = new Array();
 	var results, courseamt;
 	var yt = req.params.youtube;
@@ -39,12 +39,9 @@ exports.youtubeBool = function(req, res) { // issue -- the param in this REST UR
 		conn.query('SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, oer_analytics_youtube.totalviews AS youtube_views FROM content_type_course RIGHT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value',
 			function(err, rows, fields) {
 				if (err) throw err;
-				// console.log('Result is: ', rows[0].solution);
-				// console.log('There are ', rows.length, 'nodes of type course');
 				courseamt = rows.length;
 				results = rows;
 				console.log(results);
-				//course_details.push({number_of_courses:courseamt}); // hmm
 				for (row in results) {
 					course_details.push(results[row]);
 				}
@@ -105,16 +102,6 @@ exports.findName = function(req, res) {
 		}
 	)
 }
-
-
-// license possibilities:
-// PDNL: http://creativecommons.org/publicdomain/zero/1.0/
-// BY: http://creativecommons.org/licenses/by/3.0/
-// BY-SA: http://creativecommons.org/licenses/by-sa/3.0/
-// BY-NC: http://creativecommons.org/licenses/by-nc/3.0/
-// BY-NC-SA: http://creativecommons.org/licenses/by-nc-sa/3.0/
-// BY-NC-ND: http://creativecommons.org/licenses/by-nc-nd/3.0/
-// All Rights Reserved: ""
 
 
 exports.findByLicense = function(req, res) {
