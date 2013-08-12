adadd5c43b29ba9113c01e17303119ed606d54eb
jczetta <jczetta@gmail.com>
2013-08-12 12:54:35 -0400

    added path to all route queries

--- routes/courses.js
+++ routes/courses.js
@@ -39,7 +39,7 @@ exports.youtubeBool = function(req, res) { // issue -- the param in this REST UR
 	var yt = req.params.youtube;
 	conn.query('USE ' + DATABASE);
 	if (yt == 'vids') {
-		conn.query('SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid RIGHT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value',
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid RIGHT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -53,7 +53,7 @@ exports.youtubeBool = function(req, res) { // issue -- the param in this REST UR
 		);
 	}
 	else if (yt == 'no-vids') {
-		conn.query('SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid = content_type_course.nid WHERE oer_analytics_youtube.course_nid IS null AND field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value',
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid = content_type_course.nid WHERE oer_analytics_youtube.course_nid IS null AND field_course_code_value IS NOT NULL GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -74,7 +74,7 @@ exports.filterName = function(req, res) {
 	var course_details = new Array();
 	conn.query('USE ' + DATABASE);
 	var filter = req.params.searchterm;
-	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value LIKE '" + filter + "%' GROUP BY content_type_course.field_course_code_value",
+	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value LIKE '" + filter + "%' GROUP BY content_type_course.field_course_code_value",
 		function(err, rows, fields) {
 			if (err) throw err;
 			results = rows;
@@ -93,7 +93,7 @@ exports.findName = function(req, res) {
 	var course_details = new Array();
 	conn.query('USE ' + DATABASE);
 	var filter = req.params.searchterm;
-	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value LIKE '" + filter + "' GROUP BY content_type_course.field_course_code_value",
+	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE field_course_code_value LIKE '" + filter + "' GROUP BY content_type_course.field_course_code_value",
 		function(err, rows, fields) {
 			if (err) throw err;
 			results = rows;
@@ -111,7 +111,7 @@ exports.findByCreator = function(req, res) {
 	var course_details = new Array();
 	conn.query('USE ' + DATABASE);
 	var crName = req.params.namesearch;
-	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE attributionName LIKE '" + crName + "%' GROUP BY content_type_course.field_course_code_value",
+	conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, creativecommons_node.attributionName as creator, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON oer_analytics_youtube.course_nid LIKE content_type_course.nid WHERE attributionName LIKE '" + crName + "%' GROUP BY content_type_course.field_course_code_value",
 		function(err, rows, fields) {
 			if (err) throw err;
 			results = rows;
@@ -132,7 +132,7 @@ exports.findByLicense = function(req, res) {
 	conn.query('USE ' + DATABASE);
 	var lic = req.params.license;
 	if (lic == "by") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by/3.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by/3.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -145,7 +145,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "by-sa") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-sa/3.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-sa/3.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -158,7 +158,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "by-nc-sa") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc-sa/3.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc-sa/3.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -171,7 +171,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "by-nc") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc/3.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc/3.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -184,7 +184,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "by-nc-nd") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc-nd/3.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/licenses/by-nc-nd/3.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -197,7 +197,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "publicDomain") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/publicdomain/zero/1.0/' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst as path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE 'http://creativecommons.org/publicdomain/zero/1.0/' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
@@ -210,7 +210,7 @@ exports.findByLicense = function(req, res) {
 			}
 		);
 	} else if (lic == "none") {
-		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE '' GROUP BY content_type_course.field_course_code_value",
+		conn.query("SELECT content_type_course.field_course_code_value AS name, content_type_course.nid AS nid, url_alias.dst AS path, creativecommons_node.license_uri as license, oer_analytics_youtube.totalviews as youtube_views FROM content_type_course LEFT OUTER JOIN url_alias ON url_alias.src LIKE CONCAT('node/', content_type_course.nid) LEFT OUTER JOIN creativecommons_node ON content_type_course.nid = creativecommons_node.nid LEFT OUTER JOIN oer_analytics_youtube ON content_type_course.nid = oer_analytics_youtube.course_nid WHERE field_course_code_value IS NOT NULL AND license_uri LIKE '' GROUP BY content_type_course.field_course_code_value",
 			function(err, rows, fields) {
 				if (err) throw err;
 				courseamt = rows.length;
